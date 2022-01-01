078517fa4a92c059833f6418c8b2e6e42a943e4b
Jeong Ukjae <jeongukjae@gmail.com>
2022-01-01 14:17:17 +0900

    add initial code to parse all input sentence

--- nori/tokenizer.cc
+++ nori/tokenizer.cc
@@ -3,25 +3,31 @@
 #include <darts.h>
 #include <glog/logging.h>
 
-#include <map>
 #include <queue>
+#include <unordered_map>
 #include <vector>
 
 #include "nori/protos/dictionary.pb.h"
-
-typedef Darts::DoubleArray::result_pair_type DartsResults;
 
 namespace nori {
 
 namespace internal {
 
-struct PathValue {
-  size_t cost = 0;
-  size_t lastPositionIndex = 0;
-  size_t length = 0;
+struct TrieNode {
+  size_t cost;
+  size_t lastPositionIndex;
+  size_t length;
 
-  PathValue(size_t cost, size_t lastPositionIndex, size_t length)
-      : cost(cost), lastPositionIndex(lastPositionIndex), length(length) {}
+  nori::Morpheme* morpheme;
+  std::shared_ptr<TrieNode> parent;
+
+  TrieNode(size_t cost, size_t lastPositionIndex, size_t length,
+           nori::Morpheme* morpheme, std::shared_ptr<TrieNode> parent = nullptr)
+      : cost(cost),
+        lastPositionIndex(lastPositionIndex),
+        length(length),
+        morpheme(morpheme),
+        parent(parent) {}
 };
 
 inline int getSpacePenalty(nori::POSTag tag) {
@@ -41,6 +47,8 @@ inline int getSpacePenalty(nori::POSTag tag) {
 }  // namespace internal
 
 // NoriTokenizer class
+typedef Darts::DoubleArray::result_pair_type DartsResults;
+typedef std::shared_ptr<internal::TrieNode> SharedTrieNode;
 
 absl::Status NoriTokenizer::tokenize(const std::string& text,
                                      Lattice* lattice) const {
@@ -50,40 +58,69 @@ absl::Status NoriTokenizer::tokenize(const std::string& text,
   const char* end = text.data() + length;
   std::vector<DartsResults> trieResults(maxTrieResults + 1);
 
-  std::map<int, std::vector<internal::PathValue>> bestPaths;
-  // add bos
-  bestPaths[0].push_back(internal::PathValue(0, 0, 0));
+  std::shared_ptr<internal::TrieNode> nodesByPos;
 
-  // while (current < end) {
-  const int numNodes = dictionary->getTrie()->commonPrefixSearch(
-      current, trieResults.data(), maxTrieResults,
-      static_cast<int>(current - begin));
+  auto cmp = [](SharedTrieNode left, SharedTrieNode right) {
+    return (left->lastPositionIndex > right->lastPositionIndex) ||
+           (left->cost < right->cost);
+  };
+  std::priority_queue<SharedTrieNode, std::vector<SharedTrieNode>,
+                      decltype(cmp)>
+      nodes(cmp);
+  nodes.emplace(new internal::TrieNode(0, 0, 0, nullptr));  // bos node;
 
-  if (numNodes > maxTrieResults)
-    return absl::InternalError("Cannot search trie");
-  LOG(INFO) << dictionary->getCost(0, 1794);
-  LOG(INFO) << dictionary->getCost(3555, 1794);
+  size_t minimumCost = 1e+10;
+  while (nodes.size() > 0) {
+    SharedTrieNode nodeToSearch = nodes.top();
+    nodes.pop();
 
-  if (numNodes == 0) {
-    auto category = dictionary->getCharClass(current);
-    LOG(INFO) << category << ", "
-              << dictionary->getUnkDictionary()
-                     ->morphememap()
-                     .at(category)
-                     .wordcost();
-  }
+    current = begin + nodeToSearch->lastPositionIndex;
+    if (current == end) {
+      // EOS
+      minimumCost = std::min(minimumCost, nodeToSearch->cost);
+      continue;
+    }
 
-  for (int k = 0; k < numNodes; ++k) {
-    LOG(INFO) << std::string(current, current + trieResults[k].length);
-    auto morpheme = dictionary->getTokenDictionary()->morphemelistmap().at(
-        trieResults[k].value);
-    for (int j = 0; j < morpheme.morphemes_size(); j++) {
-      // LOG(INFO) << morpheme.morphemes(j).leftid();
-      LOG(INFO) << morpheme.morphemes(j).wordcost();
-      // LOG(INFO) << morpheme.morphemes(j).rightid();
+    LOG(INFO) << "pos: " << nodeToSearch->lastPositionIndex
+              << ", cost: " << nodeToSearch->cost
+              << ", str: " << std::string(current, end);
+
+    const int numNodes = dictionary->getTrie()->commonPrefixSearch(
+        current, trieResults.data(), maxTrieResults,
+        static_cast<int>(end - current));
+
+    if (numNodes > maxTrieResults)
+      return absl::InternalError("Cannot search trie");
+
+    if (numNodes == 0) {
+      // TODO(jeongukjae): space penalty
+      auto category = dictionary->getCharClass(current);
+      auto morpheme =
+          dictionary->getUnkDictionary()->morphememap().at(category);
+
+      // TODO(jeongukjae): connection cost
+      nodes.emplace(new internal::TrieNode(
+          nodeToSearch->cost + morpheme.wordcost(),
+          nodeToSearch->lastPositionIndex + 1, 1, &morpheme, nodeToSearch));
+    }
+
+    for (int k = 0; k < numNodes; ++k) {
+      // TODO(jeongukjae): connection cost
+      auto morphemelist =
+          dictionary->getTokenDictionary()->morphemelistmap().at(
+              trieResults[k].value);
+      for (int j = 0; j < morphemelist.morphemes_size(); j++) {
+        auto morpheme = morphemelist.morphemes(j);
+
+        nodes.emplace(new internal::TrieNode(
+            nodeToSearch->cost + morpheme.wordcost(),
+            nodeToSearch->lastPositionIndex + trieResults[k].length,
+            trieResults[k].length, &morpheme, nodeToSearch));
+      }
     }
   }
-  // }
+
+  LOG(INFO) << minimumCost;
 
   return absl::OkStatus();
 }
