6a1f7a4638387753a11bb4ae258b06bac52778dc
Jeong Ukjae <JeongUkJae@gmail.com>
2022-01-06 20:07:33 +0900

    update string ops

--- nori/graphviz_visualize.cc
+++ nori/graphviz_visualize.cc
@@ -1,25 +1,29 @@
 #include "nori/graphviz_visualize.h"
 
+#include "absl/strings/str_cat.h"
+
 namespace nori {
 
 void GraphvizVisualizer::reset() {
-  this->data =
-      ("digraph " + this->graphName +
-       " {\n"
-       "  graph [fontsize=30 labelloc=\"t\" label=\"\" splines=true "
-       "overlap=false rankdir = \"LR\"];\n"
-       "  edge [fontname=\"" +
-       this->fontName +
-       "\" fontcolor=\"red\" color=\"#606060\"]\n"
-       "  node [style=\"filled\" fillcolor=\"#e8e8f0\" shape=\"Mrecord\" "
-       "fontname=\"" +
-       this->fontName +
-       "\"]\n"
-       "  init [style=invis]\n"
-       "  init -> 0.0 [label=\"" +
-       this->bosLabel + "\"]\n");
+  this->data = absl::StrCat(
+      "digraph ", this->graphName,
+      " {\n"
+      "  graph [fontsize=30 labelloc=\"t\" label=\"\" splines=true "
+      "overlap=false rankdir = \"LR\"];\n"
+      "  edge [fontname=\"",
+      this->fontName,
+      "\" fontcolor=\"red\" color=\"#606060\"]\n"
+      "  node [style=\"filled\" fillcolor=\"#e8e8f0\" shape=\"Mrecord\" "
+      "fontname=\"",
+      this->fontName,
+      "\"]\n"
+      "  init [style=invis]\n"
+      "  init -> 0.0 [label=\"",
+      this->bosLabel, "\"]\n");
 }
 
-void GraphvizVisualizer::finish() { this->data += "}"; }
+void GraphvizVisualizer::addNode() {}
+
+void GraphvizVisualizer::finish() { absl::StrAppend(&this->data, "}"); }
 
 }  // namespace nori
