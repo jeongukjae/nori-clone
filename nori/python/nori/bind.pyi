b5bb56f5859d8b7d6f06a93367d2b19879460952
Ukjae Jeong (Jay) <jeongukjae@gmail.com>
2022-01-13 23:45:15 +0900

    add type for python binding

---
+++ nori/python/nori/bind.pyi
@@ -0,0 +1,24 @@
+from typing import List, Tuple
+
+class Token:
+    surface: str
+    offset: int
+    length: int
+    leftid: int
+    rightid: int
+    postype: str
+    expr: List[Tuple[str, str]]
+    def __repr__(self) -> str: ...
+
+class Lattice:
+    tokens: List[Token]
+    sentence: str
+
+class Dictionary:
+    def __init__(self): ...
+    def load_prebuilt_dictionary(self, dir: str): ...
+    def load_user_dictionary(self, filename: str): ...
+
+class NoriTokenizer:
+    def __init__(self, dictionary: Dictionary): ...
+    def tokenize(self, input: str) -> Lattice: ...
