71acaf1f0e5e8e2056652402ba4e18c709fec7ec
Jeong Ukjae <jeongukjae@gmail.com>
2022-01-15 14:40:30 +0900

    fixlint

--- tools/comparison/NoriRunner.java
+++ tools/comparison/NoriRunner.java
@@ -1,16 +1,18 @@
 import org.apache.lucene.analysis.ko.KoreanAnalyzer;
 import org.apache.lucene.analysis.ko.KoreanTokenizer;
 import org.apache.lucene.analysis.ko.POS;
+
 import java.util.HashSet;
 import java.util.Set;
 
 public class NoriRunner {
-  public static void main (String[] args) {
-    Set<POS.Tag> stopTags = new HashSet<>();
-    KoreanAnalyzer analyzer = new KoreanAnalyzer(null, KoreanTokenizer.DecompoundMode.DISCARD, stopTags, false);
+    public static void main(String[] args) {
+        Set<POS.Tag> stopTags = new HashSet<>();
+        KoreanAnalyzer analyzer =
+                new KoreanAnalyzer(null, KoreanTokenizer.DecompoundMode.DISCARD, stopTags, false);
 
-    // TODO(jeongukjae): add analyzer code
+        // TODO(jeongukjae): add analyzer code
 
-    analyzer.close();
-  }
+        analyzer.close();
+    }
 }
