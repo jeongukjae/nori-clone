06797ee0760061bb7c2bd35ba153522733bad0b1
Ukjae Jeong (Jay) <jeongukjae@gmail.com>
2022-10-02 20:53:17 +0900

    update dictionary

--- src/tokenizer/dictionary/system.rs
+++ src/tokenizer/dictionary/system.rs
@@ -1,4 +1,5 @@
 use std::io::Read;
+use std::path::PathBuf;
 use std::{fs::File, io::BufReader, path::Path};
 
 use crate::error::Error;
@@ -15,22 +16,30 @@ pub struct SystemDictionary {
 }
 
 impl SystemDictionary {
-    pub fn load(input_path: &str) -> Result<Self, Error> {
-        let fst = match Self::load_fst(input_path ) {
+    pub fn load_from_bytes(
+        fst_buffer: Vec<u8>,
+        token_dict_buffer: Vec<u8>,
+        unk_dict_buffer: Vec<u8>,
+        conn_cost_buffer: Vec<u8>,
+    ) -> Result<Self, Error> {
+        let fst = match Map::new(fst_buffer) {
             Ok(fst) => fst,
-            Err(e) => return Err(e),
+            Err(e) => return Err(format!("Failed to load fst: ({:?})", e).into()),
         };
-        let token_dict = match Self::load_token_dictionary(input_path) {
-            Ok(f) => f,
-            Err(e) => return Err(e),
+
+        let token_dict = match from_bytes(&token_dict_buffer) {
+            Ok(dict) => dict,
+            Err(e) => return Err(format!("Failed to load token dictionary ({:?})", e).into()),
         };
-        let unk_dict = match Self::load_unk_dictionary(input_path) {
-            Ok(f) => f,
-            Err(e) => return Err(e),
+
+        let unk_dict = match from_bytes(&unk_dict_buffer) {
+            Ok(dict) => dict,
+            Err(e) => return Err(format!("Failed to load unk dictionary ({:?})", e).into()),
         };
-        let connection_cost = match Self::load_connection_cost(input_path) {
-            Ok(f) => f,
-            Err(e) => return Err(e),
+
+        let connection_cost = match from_bytes(&conn_cost_buffer) {
+            Ok(dict) => dict,
+            Err(e) => return Err(format!("Failed to load unk dictionary ({:?})", e).into()),
         };
 
         Ok(SystemDictionary {
@@ -41,103 +50,49 @@ impl SystemDictionary {
         })
     }
 
-    fn load_fst(input_path: &str) -> Result<Map<Vec<u8>>, Error> {
-        let fst_path = Path::new(input_path).join(FST_FILENAME);
-        let f = match File::open(fst_path) {
-            Ok(f) => f,
-            Err(e) => return Err(format!("Failed to open fst file: ({:?})", e).into()),
-        };
-        let mut reader = BufReader::new(f);
-        let mut buffer = Vec::new();
-
-        match reader.read_to_end(&mut buffer) {
-            Ok(_) => (),
-            Err(e) => return Err(format!("Failed to read fst file: ({:?})", e).into()),
-        };
-
-        match Map::new(buffer) {
-            Ok(fst) => Ok(fst),
+    pub fn load_from_input_directory(input_path: &str) -> Result<Self, Error> {
+        let fst_buffer = match Self::read_file(Path::new(input_path).join(FST_FILENAME)) {
+            Ok(buffer) => buffer,
             Err(e) => return Err(format!("Failed to load fst: ({:?})", e).into()),
-        }
+        };
+
+        let token_dict_buffer = match Self::read_file(Path::new(input_path).join(TOKEN_FILENAME)) {
+            Ok(buffer) => buffer,
+            Err(e) => return Err(format!("Failed to load token dictionary ({:?})", e).into()),
+        };
+
+        let unk_dict_buffer = match Self::read_file(Path::new(input_path).join(UNK_FILENAME)) {
+            Ok(buffer) => buffer,
+            Err(e) => return Err(format!("Failed to load unk dictionary ({:?})", e).into()),
+        };
+
+        let conn_cost_buffer = match Self::read_file(Path::new(input_path).join(CON_COST_FILENAME))
+        {
+            Ok(buffer) => buffer,
+            Err(e) => return Err(format!("Failed to load unk dictionary ({:?})", e).into()),
+        };
+
+        Self::load_from_bytes(
+            fst_buffer,
+            token_dict_buffer,
+            unk_dict_buffer,
+            conn_cost_buffer,
+        )
     }
 
-    fn load_token_dictionary(input_path: &str) -> Result<TokenDictionary, Error> {
-        let token_dict_path = Path::new(input_path).join(TOKEN_FILENAME);
-        let f = match File::open(token_dict_path) {
+    fn read_file(path: PathBuf) -> Result<Vec<u8>, Error> {
+        let f = match File::open(path) {
             Ok(f) => f,
-            Err(e) => return Err(format!("Failed to open token dictionary file: ({:?})", e).into()),
+            Err(e) => return Err(format!("Failed to open file: ({:?})", e).into()),
         };
         let mut reader = BufReader::new(f);
         let mut buffer = Vec::new();
 
         match reader.read_to_end(&mut buffer) {
             Ok(_) => (),
-            Err(e) => return Err(format!("Failed to read token dictionary file: ({:?})", e).into()),
+            Err(e) => return Err(format!("Failed to read file: ({:?})", e).into()),
         };
 
-        let token_dictionary = match from_bytes(&buffer) {
-            Ok(token_dictionary) => token_dictionary,
-            Err(e) => {
-                return Err(format!("Failed to deserialize token dictionary file ({:?})", e).into())
-            }
-        };
-
-        Ok(token_dictionary)
-    }
-
-    fn load_unk_dictionary(input_path: &str) -> Result<UnknownTokenDictionary, Error> {
-        let unk_dict_path = Path::new(input_path).join(UNK_FILENAME);
-        let f = match File::open(unk_dict_path) {
-            Ok(f) => f,
-            Err(e) => {
-                return Err(format!("Failed to open unknown unk dictionary file ({:?})", e).into())
-            }
-        };
-        let mut reader = BufReader::new(f);
-        let mut buffer = Vec::new();
-
-        match reader.read_to_end(&mut buffer) {
-            Ok(_) => (),
-            Err(e) => {
-                return Err(format!("Failed to read unknown unk dictionary file ({:?})", e).into())
-            }
-        };
-
-        let unk_dictionary = match from_bytes(&buffer) {
-            Ok(unk_dictionary) => unk_dictionary,
-            Err(e) => {
-                return Err(format!(
-                    "Failed to deserialize unknown unk dictionary file ({:?})",
-                    e
-                )
-                .into())
-            }
-        };
-
-        Ok(unk_dictionary)
-    }
-
-    fn load_connection_cost(input_path: &str) -> Result<ConnectionCost, Error> {
-        let connection_cost_path = Path::new(input_path).join(CON_COST_FILENAME);
-        let f = match File::open(connection_cost_path) {
-            Ok(f) => f,
-            Err(e) => return Err(format!("Failed to open connection cost file ({:?})", e).into()),
-        };
-        let mut reader = BufReader::new(f);
-        let mut buffer = Vec::new();
-
-        match reader.read_to_end(&mut buffer) {
-            Ok(_) => (),
-            Err(e) => return Err(format!("Failed to read connection cost file ({:?})", e).into()),
-        };
-
-        let connection_cost = match from_bytes(&buffer) {
-            Ok(connection_cost) => connection_cost,
-            Err(e) => {
-                return Err(format!("Failed to deserialize connection cost file ({:?})", e).into())
-            }
-        };
-
-        Ok(connection_cost)
+        Ok(buffer)
     }
 }
--- src/tokenizer/mod.rs
+++ src/tokenizer/mod.rs
@@ -1,3 +1,7 @@
 pub mod dictionary;
+pub mod tokenizer;
 
 pub use dictionary::DictionaryBuilder;
+pub use dictionary::SystemDictionary;
+pub use dictionary::UserDictionary;
+pub use tokenizer::NoriTokenizer;
