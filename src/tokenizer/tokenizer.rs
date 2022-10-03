d9a4883b6cddd8d3506951a46e5757e9ae7e6004
Ukjae Jeong (Jay) <jeongukjae@gmail.com>
2022-10-03 16:02:37 +0900

    wip: adding unk dict search

--- src/tokenizer/tokenizer.rs
+++ src/tokenizer/tokenizer.rs
@@ -1,9 +1,10 @@
-use crate::dictionary::{Morpheme, POSTag};
+use crate::dictionary::{CategoryDefinition, CharacterClass, Morpheme, POSTag};
 use crate::{error::Error, SystemDictionary, UserDictionary};
 use crate::{utils, CommonPrefix};
 use fst::IntoStreamer;
 use fst::Streamer;
 use log::error;
+use unicode_script::ScriptExtension;
 
 pub struct NoriTokenizer {
     system_dictionary: SystemDictionary,
@@ -31,13 +32,11 @@ impl NoriTokenizer {
             (0..(end + 1)).into_iter().map(|_| Vec::new()).collect();
 
         nodes_by_position[0].push(FSTNode {
-            unique_node_id: 0,
             cost: 0,
             last_position_index: 0,
             word_length: 0,
             morpheme: self.system_dictionary.bos_eos_morpheme.clone(),
         }); // Add bos node
-        let mut node_id = 1;
 
         while start + offset < end {
             current = start + offset;
@@ -60,36 +59,35 @@ impl NoriTokenizer {
             let query = CommonPrefix::new(input_text, current);
 
             // TODO: find user dictionary
-
-            let mut system_stream = self.system_dictionary.fst.search(query).into_stream();
-            let mut is_found = false;
-            while let Some((k, v)) = system_stream.next() {
-                is_found = true;
-
-                for morpheme in &self.system_dictionary.token_dictionary.morphemes[v as usize] {
-                    let (parent_index, connection_cost) =
-                        self.select_parent_node(&nodes_by_position[offset], morpheme);
-                    let parent = &nodes_by_position[offset][parent_index as usize];
-
-                    let space_cost = Self::get_space_penalty(morpheme, num_spaces);
-                    let word_cost = morpheme.word_cost as i32;
-                    let cost = parent.cost + word_cost + connection_cost + space_cost;
-                    let word_length = k.len() as i32;
-
-                    let last_position_index = parent.last_position_index + num_spaces + word_length;
-
-                    nodes_by_position[last_position_index as usize].push(FSTNode {
-                        cost: cost,
-                        unique_node_id: node_id,
-                        last_position_index: last_position_index,
-                        word_length: word_length,
-                        morpheme: morpheme.clone(),
-                    });
+            let current_ch_cls = match self
+                .system_dictionary
+                .unk_dictionary
+                .get_character_class(input_bytes, current)
+            {
+                Ok(ch_cls) => ch_cls,
+                Err(e) => {
+                    return Err(format!(
+                        "Unknown character cls: `{}`, {}",
+                        input_bytes[current],
+                        e.description()
+                    )
+                    .into())
                 }
-            }
+            };
 
-            if !is_found {
-                // TODO: unk
+            let current_ch_def = self.system_dictionary.unk_dictionary.invoke_map[&current_ch_cls];
+
+            let is_found =
+                self.find_system_dictionary(&query, offset, num_spaces, &mut nodes_by_position);
+            if !is_found || current_ch_def.invoke == 1 {
+                self.find_unk_dictionary(
+                    input_text,
+                    offset,
+                    num_spaces,
+                    &mut nodes_by_position,
+                    current_ch_cls,
+                    current_ch_def,
+                )
             }
 
             offset += num_spaces as usize;
@@ -97,6 +95,72 @@ impl NoriTokenizer {
         }
 
         Ok(lattice)
+    }
+
+    fn find_system_dictionary(
+        &self,
+        query: &CommonPrefix,
+        offset: usize,
+        num_spaces: i32,
+        nodes_by_position: &mut Vec<Vec<FSTNode>>,
+    ) -> bool {
+        let mut system_stream = self.system_dictionary.fst.search(query).into_stream();
+        let mut is_found = false;
+
+        while let Some((k, v)) = system_stream.next() {
+            is_found = true;
+            for morpheme in &self.system_dictionary.token_dictionary.morphemes[v as usize] {
+                let (parent_index, connection_cost) =
+                    self.select_parent_node(&nodes_by_position[offset], morpheme);
+                let parent = &nodes_by_position[offset][parent_index as usize];
+
+                let space_cost = Self::get_space_penalty(morpheme, num_spaces);
+                let word_cost = morpheme.word_cost as i32;
+                let cost = parent.cost + word_cost + connection_cost + space_cost;
+                let word_length = k.len() as i32;
+
+                let last_position_index = parent.last_position_index + num_spaces + word_length;
+
+                nodes_by_position[last_position_index as usize].push(FSTNode {
+                    cost: cost,
+                    last_position_index: last_position_index,
+                    word_length: word_length,
+                    morpheme: morpheme.clone(),
+                });
+            }
+        }
+
+        is_found
+    }
+
+    fn find_unk_dictionary(
+        &self,
+        input_text: &str,
+        offset: usize,
+        num_spaces: i32,
+        nodes_by_position: &mut Vec<Vec<FSTNode>>,
+        current_ch_cls: CharacterClass,
+        current_ch_def: CategoryDefinition,
+    ) {
+        let text = &input_text[offset..];
+        let ext = ScriptExtension::for_str(text);
+        //   int length = internal::groupingUnknownCharacters(
+        //       current, end, category, dictionary, charDef->group() == 1);
+
+        //   const nori::protos::Morpheme* morpheme =
+        //       &dictionary->getUnkTokens()->morpheme_map().at(category);
+        //   const int wordCost = morpheme->word_cost();
+        //   auto spaceCost = internal::getSpacePenalty(morpheme, numSpaces);
+        //   int connectionCost;
+        //   auto parent = internal::selectParent(nodesByPos[offset], morpheme,
+        //                                        this->dictionary, connectionCost);
+
+        //   auto lastPositionIndex = parent->lastPositionIndex + numSpaces + length;
+        //   auto lastNodeId = nodeId;
+        //   auto cost = parent->cost + wordCost + connectionCost + spaceCost;
+
+        //   nodesByPos[lastPositionIndex].emplace_back(
+        //       nodeId++, cost, lastPositionIndex, length, morpheme, parent);
     }
 
     fn get_space_penalty(morpheme: &Morpheme, num_spaces: i32) -> i32 {
@@ -148,6 +212,8 @@ impl NoriTokenizer {
 
         (result_index, min_cost)
     }
+
+    fn group_unknown_chars(&self, x: &str) {}
 }
 
 #[derive(Debug)]
@@ -167,7 +233,6 @@ pub struct Node {}
 #[derive(Debug, Clone)]
 pub struct FSTNode {
     cost: i32,
-    unique_node_id: i32,
 
     word_length: i32,
     last_position_index: i32,
