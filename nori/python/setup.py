a3a82daf6a2afc6ddd84d9c66f3fe551ee36db4f
Jeong Ukjae <jeongukjae@gmail.com>
2022-10-21 22:27:38 +0900

    update python package version

--- nori/python/setup.py
+++ nori/python/setup.py
@@ -17,7 +17,7 @@ with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'README.md'),
 
 setup(
     name="nori-clone",
-    version="1.0.0",
+    version="1.1.0",
     python_requires=">=3.7",
     packages=find_packages(),
     url="https://github.com/jeongukjae/nori-clone",
